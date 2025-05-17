93c35cb8ae49ac877c9ff5d519a337bddeaaad22
Vijender P <contactvij09@gmail.com>
2025-05-17 14:31:14 +0530

    Added 2 new endpoints

--- routes/eventbotRoutes.js
+++ routes/eventbotRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const eventbotController = require('../controllers/eventbotController');
-
-// Start a new quiz attempt
 router.post('/registerUser', eventbotController.addRegisteredUser);
 router.post('/getRegisteredUserList', eventbotController.getRegisteredUserList);
 router.post('/checkinUser', eventbotController.checkinUser);
 router.post('/chat/save', eventbotController.saveChat);
 router.post('/chat/list', eventbotController.getChatList);
 router.post('/validateRegisteredEmailId', eventbotController.validateRegisteredUserEmailId);
+router.post('/getRegisteredUserCount', eventbotController.getRegisteredUserCount);
+router.post('/getCheckedInUserCount', eventbotController.getCheckedInUserCount);
 module.exports = router;
